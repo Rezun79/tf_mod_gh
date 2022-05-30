85af5b5517ded00d3e195615d5ecf30462728fb8
rezun79 <rezun79@gmail.com>
2022-05-31 00:17:30 +0200

    test

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "github_repository" "AZ_RG" {
   description = "Repo for ${local.rg_name} code"
   visibility = "public"
 }
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [github_repository.AZ_RG]
+
+  create_duration = "10s"
+}
--- providers.tf
+++ providers.tf
@@ -4,10 +4,17 @@ terraform {
       source = "integrations/github"
       version = "4.26.0"
     }
+    time = {
+      source = "hashicorp/time"
+      version = "0.7.2"
+    }
   }
 }
 
 provider "github" {
   token = local.github_token
   owner = local.github_organization
+}
+provider "time" {
+  # Configuration options
 }
