e33dc2aad495ef790f070e1e0b92776b08ea76ee
rezun79 <rezun79@gmail.com>
2022-05-30 23:18:09 +0200

    test

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "AZ_RG_repo" {
+resource "github_repository" "AZ_R" {
   name        = local.rg_name
   description = "Repo for ${local.rg_name} code"
   visibility = "public"
