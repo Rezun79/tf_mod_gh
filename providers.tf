0a6b147336f78e9e5db513e1fdf2f9759a75ea8e
Rezun79 <rezun79@gmail.com>
2022-06-02 15:04:03 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,26 +1,26 @@
-terraform {
-  required_providers {
-    github = {
-      source = "integrations/github"
-      version = "4.26.0"
-    }
-#     time = {
-#       source = "hashicorp/time"
-#       version = "0.7.2"
+# terraform {
+#   required_providers {
+#     github = {
+#       source = "integrations/github"
+#       version = "4.26.0"
 #     }
-  }
-}
-
-# provider "github" {
-#   token = local.github_token
-#   owner = local.github_organization
+# #     time = {
+# #       source = "hashicorp/time"
+# #       version = "0.7.2"
+# #     }
+#   }
 # }
 
-provider "github" {
-  owner = local.github_organization
-  app_auth {
-    id              = local.github_app_id
-    installation_id = local.github_app_instalation_id
-    pem_file        = local.github_app_priv_key
-  }
-}
+# # provider "github" {
+# #   token = local.github_token
+# #   owner = local.github_organization
+# # }
+
+# provider "github" {
+#   owner = local.github_organization
+#   app_auth {
+#     id              = local.github_app_id
+#     installation_id = local.github_app_instalation_id
+#     pem_file        = local.github_app_priv_key
+#   }
+# }
