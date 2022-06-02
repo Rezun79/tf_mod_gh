c788112340577c946c12e4f6f6338aac49f5cd7a
Rezun79 <rezun79@gmail.com>
2022-06-02 15:17:23 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,15 @@
-# terraform {
-#   required_providers {
-#     github = {
-#       source = "integrations/github"
-#       version = "4.26.0"
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "4.26.0"
+    }
+#     time = {
+#       source = "hashicorp/time"
+#       version = "0.7.2"
 #     }
-# #     time = {
-# #       source = "hashicorp/time"
-# #       version = "0.7.2"
-# #     }
-#   }
-# }
+  }
+}
 
 # # provider "github" {
 # #   token = local.github_token
