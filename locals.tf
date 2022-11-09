a27e0b6be31a304801a0aa3739f2fb57d62d09c7
Rezun79 <rezun79@gmail.com>
2022-11-09 20:29:04 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   rg_name = var.rg_name
-  #github_token = var.github_token
-  github_app_id = var.github_app_id
-  github_app_instalation_id = var.github_app_instalation_id
-  github_app_priv_key = var.github_app_priv_key
+  github_token = var.github_token
+#   github_app_id = var.github_app_id
+#   github_app_instalation_id = var.github_app_instalation_id
+#   github_app_priv_key = var.github_app_priv_key
   github_organization = var.github_organization
 }
