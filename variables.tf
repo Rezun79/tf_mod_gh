54cc386f03cd3d04fe963a4256effe94f73ac4ef
Rezun79 <rezun79@gmail.com>
2022-11-09 20:33:09 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,20 +6,20 @@ variable "github_token" {
   sensitive = true
 }
 
-variable "github_app_id" {
-  type = string
-  sensitive = true
-}
+# variable "github_app_id" {
+#   type = string
+#   sensitive = true
+# }
 
-variable "github_app_instalation_id" {
-  type = string
-  sensitive = true
-}
+# variable "github_app_instalation_id" {
+#   type = string
+#   sensitive = true
+# }
 
-variable "github_app_priv_key" {
-  type = string
-  sensitive = true
-}
+# variable "github_app_priv_key" {
+#   type = string
+#   sensitive = true
+# }
 
 variable "github_organization" {
   type = string
