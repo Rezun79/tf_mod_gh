8987bf654fc8582d00b8533098ef3507b8f50a62
Rezun79 <rezun79@gmail.com>
2022-11-09 20:38:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ resource "github_repository" "AZ_RG" {
   auto_init = true
 }
 
-# resource "github_repository_file" "gitignore" {
-#   repository          = github_repository.AZ_RG.name
-#   branch              = "main"
-#   file                = ".gitignore"
-#   content             = "**/*.tfstate"
-#   commit_message      = "Managed by Terraform"
-#   commit_author       = "Terraform User"
-#   commit_email        = "terraform@example.com"
-#   overwrite_on_create = true
-# }
+resource "github_repository_file" "gitignore" {
+  repository          = github_repository.AZ_RG.name
+  branch              = "main"
+  file                = ".gitignore"
+  content             = "**/*.tfstate"
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "terraform@example.com"
+  overwrite_on_create = true
+}
 
 # resource "time_sleep" "wait_30_seconds" {
 #   depends_on = [github_repository.AZ_RG]
